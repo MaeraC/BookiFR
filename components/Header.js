f76b9bce392cea3cbc92ddd2ada63e6b8831e25d
Maera <baeitsmae@gmail.com>
2022-12-11 23:19:22 +0100

    Header done

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+const header = document.querySelector(".header");
+const logo = document.createElement("img");
+const nav = document.createElement("nav");
+const heberg = document.createElement("a");
+const activities = document.createElement("a");
+
+logo.setAttribute("src", "../assets/logo/Booki.png");
+logo.setAttribute("alt", "Logo Booki");
+
+logo.classList.add("logo");
+heberg.classList.add("heberg-nav");
+activities.classList.add("activities-nav");
+
+heberg.textContent = "Hébergements";
+activities.textContent = "Activités";
+
+heberg.setAttribute("href", "");
+activities.setAttribute("href", "");
+
+
+header.appendChild(logo);
+header.appendChild(nav);
+nav.appendChild(heberg);
+nav.appendChild(activities);
